9dd62b8a350b3be03221bfc7397115ec46b796c0
Anthony Fiset <tonyfise@my.yorku.ca>
2022-12-08 15:58:07 -0500

    Did Part A for Q2

---
+++ Q2/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Mod_Frac myFraction = new Mod_Frac(9, 9);
+
+    }
+
+}
---
+++ Q2/Mod_Frac.java
@@ -0,0 +1,22 @@
+class Mod_Frac {
+    
+    //Part A
+
+    //Integer for numerator(n) & denominator(d)
+    private int numerator;
+    private int denominator;
+
+    public Mod_Frac(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        System.out.println("Your Numerator is: " +  numerator);
+        System.out.println("Your Denominator is: " + denominator);
+    }
+
+    //Part B
+
+    public Mod_Frac(double number) {
+        
+    }
+
+}
